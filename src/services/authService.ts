27c9be7acbba8309c4e7b4b2a2f74d0e45270734
rafaelhosaka <hosakarafael@gmail.com>
2024-11-22 10:38:29 +0900

    Add Alert and validation to login and create

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -14,6 +14,34 @@ export const authenticate = async (
     },
   });
 
+  return res.json();
+};
+
+export const register = async (
+  name: string,
+  email: string,
+  password: string
+): Promise<RegisterResponse> => {
+  const res = await fetch(`${serviceURL}/register`, {
+    body: JSON.stringify({ name, email, password }),
+    method: "POST",
+    headers: {
+import { AuthenticationResponse, RegisterResponse } from "@/lib/definitions";
+
+const serviceURL: string = process.env.NEXT_PUBLIC_API_URL + "auth";
+
+export const authenticate = async (
+  email: string,
+  password: string
+): Promise<AuthenticationResponse> => {
+  const res = await fetch(`${serviceURL}/authenticate`, {
+    body: JSON.stringify({ email, password }),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return await res.json();
 };
 
