def390b5f260cc5381f03af5c8eabc4e43f0245d
rafaelhosaka <hosakarafael@gmail.com>
2025-01-03 21:18:27 +0900

    Initial commit for translation

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -6,8 +6,10 @@ import { useUser } from "@/context/userContext";
 import { authenticate } from "@/services/authService";
 import { redirect, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
+  const t = useTranslations("LoginPage");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { updateToken, updateUser } = useUser();
@@ -87,60 +89,70 @@ export default function Page() {
       <Logo />
       <div className="flex items-center h-screen justify-center gap-3 dark:text-white">
         <div className="flex flex-col gap-5 sm:flex-row">
-          <h1 className="text-6xl">Login</h1>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-96">
-            <label className="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-              </svg>
-              <input
-                type="text"
-                className="grow"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                autoFocus
-              />
-            </label>
-            <label className="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                  clipRule="evenodd"
+          <h1 className="text-6xl">{t("title")}</h1>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-1 w-96">
+            <label className="form-control w-96">
+              <div className="label">
+                <span>Email</span>
+              </div>
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder={t("emailPlaceholder")}
+                  onChange={(e) => setEmail(e.target.value)}
+                  autoFocus
                 />
-              </svg>
-              <input
-                type="password"
-                className="grow"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              </label>
             </label>
+
+            <label className="form-control w-96">
+              <div className="label">
+                <span>Password</span>
+              </div>
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="password"
+                  className="grow"
+                  placeholder={t("passwordPlaceholder")}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </label>
+
             <Alert
               show={isAlertVisible}
               type="error"
               message={errorMessage}
               onClose={() => setIsAlertVisible(false)}
             />
-            <div className="flex justify-end gap-3">
+            <div className="flex justify-end gap-3  mt-3">
               <a
-                href={
-                  continueTo ? `/create?continueTo=${continueTo}` : "/create"
-                }
+                href={continueTo ? `create?continueTo=${continueTo}` : "create"}
                 className="btn btn-neutral dark:btn-outline text-xl dark:text-white rounded-full"
               >
-                Create
+                {t("createBtn")}
               </a>
 
               <button
@@ -150,7 +162,7 @@ export default function Page() {
                 {loading ? (
                   <span className="loading loading-spinner loading-lg"></span>
                 ) : (
-                  "Login"
+                  t("loginBtn")
                 )}
               </button>
             </div>
